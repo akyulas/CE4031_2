8a79fbfea37600e6465ac9a9b75c86a27782373e
akyulas <bolshack_fire@hotmail.com>
2019-10-27 15:52:15 +0800

    Added in the needed SQL commands

---
+++ sql_commands/bulk_insert.sql
@@ -0,0 +1,31 @@
+copy customer
+from '/private/tmp/CE4031_datasets/customer.csv'
+delimiter '|';
+
+copy lineitem
+from '/private/tmp/CE4031_datasets/lineitem.csv'
+delimiter '|';
+
+copy nation
+from '/private/tmp/CE4031_datasets/nation.csv'
+delimiter '|';
+
+copy orders
+from '/private/tmp/CE4031_datasets/orders.csv'
+delimiter '|';
+
+copy part
+from '/private/tmp/CE4031_datasets/part.csv'
+delimiter '|';
+
+copy partsupp
+from '/private/tmp/CE4031_datasets/partsupp.csv'
+delimiter '|';
+
+copy region
+from '/private/tmp/CE4031_datasets/region.csv'
+delimiter '|';
+
+copy supplier
+from '/private/tmp/CE4031_datasets/supplier.csv'
+delimiter '|';
---
+++ sql_commands/check_tables_values.sql
@@ -0,0 +1,15 @@
+select * from customer;
+
+select * from lineitem;
+
+select * from nation;
+
+select * from orders;
+
+select * from part;
+
+select * from partsupp;
+
+select * from region;
+
+select * from supplier;
