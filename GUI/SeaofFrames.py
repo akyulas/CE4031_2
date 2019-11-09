4b7d7680f060b7d24a3075bac6d93ffa74ee76e7
zhien95 <37785503+zhien95@users.noreply.github.com>
2019-11-09 17:57:57 +0800

    Added sample networkx graphs into QTPage

--- GUI/SeaofFrames.py
+++ GUI/SeaofFrames.py
@@ -1,10 +1,21 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+import networkx as nx
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+
+
+
 
 
 LARGE_FONT = ("Verdana",12)
 HEIGHT = 500
 WIDTH = 600
+
+
+
 
 def set_input(textbox, value):
     textbox.config(state='normal')
@@ -18,19 +29,20 @@ def getQueryPlan(q1,q2,r1,r2):
     set_input(r1,plan)
     set_input(r2,plan2)
     #get query plan code
+    
+    
+    
 
 
 
 class SeaofFrames(tk.Tk):
-
-    
     def __init__(self,*args,**kwargs):
         tk.Tk.__init__(self,*args,**kwargs)
-        
-        container = tk.Frame(self)
+        container = tk.Frame(self,width=100, height=100, background="bisque")
         container.pack(side = "top", fill = "both", expand = True)
         container.grid_rowconfigure(0,weight=1)
         container.grid_columnconfigure(0,weight=1)
+   
         self.frames = {}
         for F in (HomePage, QueryPage, QPTPage):
             frame = F(container,self)
@@ -52,13 +64,16 @@ class HomePage(tk.Frame):
     
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        
         menu_frame = tk.Frame(self,  bg='#0082FF')
         menu_frame.pack()
+        title_frame = tk.Frame(self,  bg='#FF2E00')
+        title_frame.pack(side ='left')
+        
      
        
 
-        title_frame = tk.Frame(self,  bg='#FF2E00')
-        title_frame.pack(side ='left')
+        
         button = tk.Button(menu_frame, text = "Home Page",
                             command=lambda:controller.show_frame(HomePage))
         button.pack(side = 'left', pady=10,padx=10, fill ='both')
@@ -79,7 +94,7 @@ class QueryPage(HomePage):
     
     def __init__(self,parent,controller):
         HomePage.__init__(self,parent,controller)
-
+        
         frame = tk.Frame(self,bg='#900C3F')
         frame.place(relx = 0.05, rely=0.1, relwidth=0.40, relheight=0.85)
         
@@ -129,11 +144,33 @@ class QPTPage(HomePage):
     title = "Query Tree Page"
     def __init__(self,parent,controller):
         HomePage.__init__(self,parent,controller)
-
         
-app = SeaofFrames()
-#Set Canvas and background
+        #networkx graph1
+        f1 = plt.figure(figsize=(5,5))
+        a1 = f1.add_subplot(111)
+        G1 = nx.gnp_random_graph(20,0.5)
+        nx.draw_networkx(G1,ax=a1)
+        canvas1 = FigureCanvasTkAgg(f1,self)
+        canvas1.get_tk_widget().pack(side='left', fill =tk.BOTH, expand = True)
+        
+        #networkx graph2
+        f2 = plt.figure(figsize=(5,5))
+        a2 = f2.add_subplot(111)
+        G2 = nx.gnp_random_graph(10,0.3)
+        nx.draw_networkx(G2,ax=a2)
+        canvas2 = FigureCanvasTkAgg(f2,self)
+        canvas2.get_tk_widget().pack(side='left', fill =tk.BOTH, expand = True)
+       
+      
+        
+       
+       
+   
+       
 
+
+app = SeaofFrames()
+app.minsize(width = WIDTH, height = HEIGHT)
 
 
 
